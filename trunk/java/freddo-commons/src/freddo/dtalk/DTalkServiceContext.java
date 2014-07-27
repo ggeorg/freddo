c28c7dbd3fa796f8dc16634664808540cc185123
georgopoulos.georgios@gmail.com <georgopoulos.georgios@gmail.com@6136b987-0273-0ace-f82f-3b192ca142bd>
2014-07-27 07:34:06 +0000

    ).6.17

--- trunk/java/freddo-commons/src/freddo/dtalk/DTalkServiceContext.java
+++ trunk/java/freddo-commons/src/freddo/dtalk/DTalkServiceContext.java
@@ -1,5 +1,9 @@
 package freddo.dtalk;
 
+/**
+ * Interface to provide a common application context to all DTalk services on
+ * all platforms.
+ */
 public interface DTalkServiceContext {
 
   void runOnUiThread(Runnable r);
