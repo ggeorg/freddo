e27e60667cdf31bd4acdeef28444a851f275e85e
georgopoulos.georgios <georgopoulos.georgios@gmail.com>
2014-07-27 07:32:29 +0000

    MessageBus is using LOG

--- java/freddo-commons/src/com/arkasoft/freddo/messagebus/ListenerList.java
+++ java/freddo-commons/src/com/arkasoft/freddo/messagebus/ListenerList.java
@@ -18,153 +18,159 @@ package com.arkasoft.freddo.messagebus;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+import freddo.dtalk.util.LOG;
+
 /**
  * Class for listener lists.
  * 
  * @param <T>
  */
 final class ListenerList<T> implements Iterable<T> {
-   // Node containing a listener in the list
-   private class Node {
-      private Node previous;
-      private Node next;
-      private T listener;
+  private static final String TAG = LOG.tag(ListenerList.class);
 
-      public Node(Node previous, Node next, T listener) {
-         this.previous = previous;
-         this.next = next;
-         this.listener = listener;
-      }
-   }
+  // Node containing a listener in the list
+  private class Node {
+    private Node previous;
+    private Node next;
+    private T listener;
 
-   // Node iterator
-   private class NodeIterator implements Iterator<T> {
-      private Node node;
+    public Node(Node previous, Node next, T listener) {
+      this.previous = previous;
+      this.next = next;
+      this.listener = listener;
+    }
+  }
 
-      public NodeIterator() {
-         this.node = first;
-      }
+  // Node iterator
+  private class NodeIterator implements Iterator<T> {
+    private Node node;
 
-      public boolean hasNext() {
-         return (node != null);
-      }
+    public NodeIterator() {
+      this.node = first;
+    }
 
-      public T next() {
-         if (node == null) {
-            throw new NoSuchElementException();
-         }
-
-         T listener = node.listener;
-         node = node.next;
-
-         return listener;
-      }
-
-      public void remove() {
-         throw new UnsupportedOperationException();
-      }
-   }
-
-   // First node in the list (we don't maintain a reference to the last
-   // node, since we need to walk the list looking for duplicates on add)
-   private Node first = null;
-
-   /**
-    * Adds a listener to the list, if it has not previously been added.
-    * 
-    * @param listener
-    */
-   public void add(T listener) {
-     try {
-      if (listener == null) {
-         throw new IllegalArgumentException("listener is null.");
-      }
-
-      Node node = first;
-
-      if (node == null) {
-         first = new Node(null, null, listener);
-      } else {
-         while (node.next != null && node.listener != listener) {
-            node = node.next;
-         }
-
-         if (node.next == null && node.listener != listener) {
-            node.next = new Node(node, null, listener);
-         } else {
-            System.err.println("Duplicate listener " + listener + " added to " + this);
-            throw new RuntimeException();
-         }
-      }
-     }catch(Exception e){e.printStackTrace();}
-   }
-
-   /**
-    * Removes a listener from the list, if it has previously been added.
-    * 
-    * @param listener
-    */
-   public void remove(T listener) {
-      if (listener == null) {
-         throw new IllegalArgumentException("listener is null.");
-      }
-
-      Node node = first;
-      while (node != null && node.listener != listener) {
-         node = node.next;
-      }
-
-      if (node == null) {
-         System.err.println("Nonexistent listener " + listener + " removed from " + this);
-      } else {
-         if (node.previous == null) {
-            first = node.next;
-
-            if (first != null) {
-               first.previous = null;
-            }
-         } else {
-            node.previous.next = node.next;
-
-            if (node.next != null) {
-               node.next.previous = node.previous;
-            }
-         }
-      }
-   }
-
-   /**
-    * Tests the existence of a listener in the list.
-    * 
-    * @param listener
-    * 
-    * @return <tt>true</tt> if the listener exists in the list; <tt>false</tt>,
-    *         otherwise.
-    */
-   public boolean contains(T listener) {
-      if (listener == null) {
-         throw new IllegalArgumentException("listener is null.");
-      }
-
-      Node node = first;
-      while (node != null && node.listener != listener) {
-         node = node.next;
-      }
-
+    public boolean hasNext() {
       return (node != null);
-   }
+    }
 
-   /**
-    * Tests the emptiness of the list.
-    * 
-    * @return <tt>true</tt> if the list contains no listeners; <tt>false</tt>,
-    *         otherwise.
-    */
-   public boolean isEmpty() {
-      return (first == null);
-   }
+    public T next() {
+      if (node == null) {
+        throw new NoSuchElementException();
+      }
 
-   public Iterator<T> iterator() {
-      return new NodeIterator();
-   }
+      T listener = node.listener;
+      node = node.next;
+
+      return listener;
+    }
+
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
+  }
+
+  // First node in the list (we don't maintain a reference to the last
+  // node, since we need to walk the list looking for duplicates on add)
+  private Node first = null;
+
+  /**
+   * Adds a listener to the list, if it has not previously been added.
+   * 
+   * @param listener
+   */
+  public void add(T listener) {
+    try {
+      if (listener == null) {
+        throw new IllegalArgumentException("listener is null.");
+      }
+
+      Node node = first;
+
+      if (node == null) {
+        first = new Node(null, null, listener);
+      } else {
+        while (node.next != null && node.listener != listener) {
+          node = node.next;
+        }
+
+        if (node.next == null && node.listener != listener) {
+          node.next = new Node(node, null, listener);
+        } else {
+          LOG.e(TAG, "Duplicate listener %s added to %s", listener, this);
+          throw new RuntimeException();
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Removes a listener from the list, if it has previously been added.
+   * 
+   * @param listener
+   */
+  public void remove(T listener) {
+    if (listener == null) {
+      throw new IllegalArgumentException("listener is null.");
+    }
+
+    Node node = first;
+    while (node != null && node.listener != listener) {
+      node = node.next;
+    }
+
+    if (node == null) {
+      LOG.e(TAG, "Nonexistent listener %s removed from %s", listener, this);
+    } else {
+      if (node.previous == null) {
+        first = node.next;
+
+        if (first != null) {
+          first.previous = null;
+        }
+      } else {
+        node.previous.next = node.next;
+
+        if (node.next != null) {
+          node.next.previous = node.previous;
+        }
+      }
+    }
+  }
+
+  /**
+   * Tests the existence of a listener in the list.
+   * 
+   * @param listener
+   * 
+   * @return <tt>true</tt> if the listener exists in the list; <tt>false</tt>,
+   *         otherwise.
+   */
+  public boolean contains(T listener) {
+    if (listener == null) {
+      throw new IllegalArgumentException("listener is null.");
+    }
+
+    Node node = first;
+    while (node != null && node.listener != listener) {
+      node = node.next;
+    }
+
+    return (node != null);
+  }
+
+  /**
+   * Tests the emptiness of the list.
+   * 
+   * @return <tt>true</tt> if the list contains no listeners; <tt>false</tt>,
+   *         otherwise.
+   */
+  public boolean isEmpty() {
+    return (first == null);
+  }
+
+  public Iterator<T> iterator() {
+    return new NodeIterator();
+  }
 }
